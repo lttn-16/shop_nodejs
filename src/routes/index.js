f0162274489d76c26bb45afddf22fe9b3c525a13
nhile <lttn.16@gmail.com>
2026-05-10 21:19:23 +0700

    unblock middleware api key

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { apiKey, permissions } = require('../auth/checkAuth');
 
 // check api key
-router.use(apiKey);
+// router.use(apiKey);
 // check permissions
 router.use(permissions('0000'));
 
