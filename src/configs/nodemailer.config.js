39fbbf069f60e299375a60b9dedfab912f15f943
nhile <lttn.16@gmail.com>
2026-05-12 08:51:40 +0700

    add nodemailer test func

--- src/configs/nodemailer.config.js
+++ src/configs/nodemailer.config.js
@@ -1,12 +1,20 @@
 const nodemailer = require('nodemailer');
 
+async function getAccount() {
+    let testAccount = await nodemailer.createTestAccount();
+    console.log('--- COPY THÔNG TIN NÀY ---');
+    console.log('User:', testAccount.user);
+    console.log('Pass:', testAccount.pass);
+    console.log('---------------------------');
+}
+
 const transport = nodemailer.createTransport({
-    host: 'email-smtp.ap-southeast-1.amazonaws.com',
-    port: 465,
-    secure: true, // sử dụng SSL
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // sử dụng SSL
     auth: {
-        user: '',
-        pass: ''
+        user: 'k5l7cul2m6wotpgj@ethereal.email',
+        pass: '5WRUPcnDuNQBX95zrC'
     }
 })
 
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,14 +1,15 @@
-const { newOtp } = require('./otp.service');
-const { getTemplate } = require('./template.service');
-const transport = require('../configs/nodemailer.config');
-const { replacePlaceholder } = require('../utils')
-const { NotFoundError } = require('../core/error.response')
+const { newOtp } = require("./otp.service");
+const { getTemplate } = require("./template.service");
+const transport = require("../configs/nodemailer.config");
+const { replacePlaceholder } = require("../utils");
+const { NotFoundError } = require("../core/error.response");
+const nodemailer = require("nodemailer");
 
 const sendEmailLinkVerify = async ({
     html,
     toEmail,
-    subject = 'Xác nhận Email đăng ký!',
-    text = 'xác nhận..'
+    subject = "Xác nhận Email đăng ký!",
+    text = "xác nhận..",
 }) => {
     try {
         const mailOptions = {
@@ -16,62 +17,52 @@ const sendEmailLinkVerify = async ({
             to: toEmail,
             subject,
             text,
-            html
-        }
+            html,
+        };
 
-        transport.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                // Thay console.log bằng logger chuyên nghiệp bạn đã viết
-                return console.log(err);
-            }
-
-            console.log('Message sent::', info.messageId);
-        })
+        const info = await transport.sendMail(mailOptions);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        return info;
     } catch (error) {
         console.error(`error send Email::`, error);
         return error;
     }
-}
+};
 
-const sendEmailToken = async ({
-    email = null
-}) => {
+const sendEmailToken = async ({ email = null }) => {
     try {
         // 1. Get Token (Tạo mã OTP/Token ngẫu nhiên và lưu DB)
-        const token = await newOtp({ email })
+        const token = await newOtp({ email });
 
         // 2. Get Template (Lấy mẫu HTML từ Database)
         const template = await getTemplate({
-            tem_name: 'Template 05'
-        })
+            tem_name: "Template 05",
+        });
 
         if (!template) {
-            throw new NotFoundError('Template not found')
+            throw new NotFoundError("Template not found");
         }
 
         // 3. Replace placeholder with params (Đổ dữ liệu động vào HTML)
-        const content = replacePlaceholder(
-            template.tem_html,
-            {
-                link_verify: `http://localhost:3056/cgp/welcome-back?token=${token.otp_token}`
-            }
-        )
+        const content = replacePlaceholder(template.tem_html, {
+            link_verify: `http://localhost:3056/cgp/welcome-back?token=${token.otp_token}`,
+        });
 
         // 4. Send email (Gọi transport để gửi mail thật)
         sendEmailLinkVerify({
             html: content,
             toEmail: email,
-            subject: 'Vui lòng xác nhận địa chỉ Email đăng ký ShopDEV.com!'
-        }).catch(err => console.error(err))
+            subject: "Vui lòng xác nhận địa chỉ Email đăng ký ShopDEV.com!",
+        }).catch((err) => console.error(err));
 
-        return 1
+        return 1;
     } catch (error) {
         // Bạn có thể dùng myLogger.error ở đây để ghi log lỗi
-        console.error(error)
-        throw error
+        console.error(error);
+        throw error;
     }
-}
+};
 
 module.exports = {
     sendEmailToken,
-}
+};
